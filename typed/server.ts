f7d2e590185d4eef88ca6812226c9795c093cc20
Hannah Hoang <hannahhoanggg@gmail.com>
2023-09-29 12:10:26 -0700

    Initial commit

---
+++ typed/server.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove when used */
+import 'dotenv/config';
+import express from 'express';
+import pg from 'pg';
+import { ClientError, errorMiddleware } from './lib/index.js';
+
+const connectionString =
+  process.env.DATABASE_URL ||
+  `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
+// eslint-disable-next-line no-unused-vars -- Remove when used
+const db = new pg.Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+
+// Create paths for static directories
+const reactStaticDir = new URL('../client/dist', import.meta.url).pathname;
+const uploadsStaticDir = new URL('public', import.meta.url).pathname;
+
+app.use(express.static(reactStaticDir));
+// Static directory for file uploads server/public/
+app.use(express.static(uploadsStaticDir));
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+/**
+ * Serves React's index.html if no api route matches.
+ *
+ * Implementation note:
+ * When the final project is deployed, this Express server becomes responsible
+ * for serving the React files. (In development, the Vite server does this.)
+ * When navigating in the client, if the user refreshes the page, the browser will send
+ * the URL to this Express server instead of to React Router.
+ * Catching everything that doesn't match a route and serving index.html allows
+ * React Router to manage the routing.
+ */
+app.get('*', (req, res) => res.sendFile(`${reactStaticDir}/index.html`));
+
+app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
+});
