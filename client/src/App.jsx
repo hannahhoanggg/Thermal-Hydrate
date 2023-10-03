76d71d6e456cc027029e7466e4fb78df4918f22a
Hannah Hoang <hannahhoanggg@gmail.com>
2023-10-03 16:56:26 +0000

    Trying stuff out.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 // import { useEffect, useState } from 'react';
 import Banner from './components/Banner';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
     <div>
       <Banner />
+      <NavBar />
     </div>
   );
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <div>
+      <nav className="w-screen mx-auto bg-pink-300"></nav>
+    </div>
+  );
+}
