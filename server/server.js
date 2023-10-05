18911abed0a812b449726e5875f0f1abe4179d3f
Hannah Hoang <hannahhoanggg@gmail.com>
2023-10-05 17:33:11 +0000

    Working on server, added get and post.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 import 'dotenv/config';
 import express from 'express';
 import pg from 'pg';
+import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
 import {
   ClientError,
   errorMiddleware,
@@ -43,14 +45,14 @@ app.get('/api/products', async (req, res, next) => {
     if (!products) {
       throw new ClientError(404, 'Cannot find any products');
     }
-    res.json(products);
+    res.status(201).json(products);
   } catch (error) {
     next(error);
   }
 });
 
 // GET products based on productId
-app.get('/api/products/productId', async (req, res, next) => {
+app.get('/api/products/:productId', async (req, res, next) => {
   try {
     const productId = Number(req.params.productId);
     if (!Number.isInteger(productId) || productId <= 0) {
@@ -58,7 +60,7 @@ app.get('/api/products/productId', async (req, res, next) => {
     }
     const sql = `select *
     from "products"
-    where "productId" = '$1'
+    where "productId" = $1
     `;
     const params = [productId];
     const result = await db.query(sql, params);
@@ -69,9 +71,61 @@ app.get('/api/products/productId', async (req, res, next) => {
         `Cannot find any products with that ID ${productId}.`
       );
     }
-    res.json(products);
+    res.status(201).json(products);
   } catch (error) {
     next(error);
+  }
+});
+
+// User can create an account
+app.post('/api/sign-up', async (req, res, next) => {
+  try {
+    const { username, password, firstName, lastName, email } = req.body;
+    if (!username || !password)
+      throw new ClientError(400, 'Username and password are required fields');
+    const hashedPassword = await argon2.hash(password);
+    const sql = `
+      insert into "users" ("username", "hashedPassword", "firstName", "lastName", "email")
+      values ($1, $2, $3, $4, $5)
+      returning *
+    `;
+    const params = [username, hashedPassword, firstName, lastName, email];
+    const result = await db.query(sql, params);
+    const [user] = result.rows;
+    res.status(201).json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// User can log in
+app.post('/api/sign-in', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ClientError(401, 'Invalid login, please try again.');
+    }
+    const sql = `
+      select "userId",
+            "hashedPassword"
+        from "users"
+      where "username" = $1
+    `;
+    const params = [username];
+    const result = await db.query(sql, params);
+    const [user] = result.rows;
+    if (!user) {
+      throw new ClientError(401, 'invalid login');
+    }
+    const { userId, hashedPassword } = user;
+    if (!(await argon2.verify(hashedPassword, password))) {
+      throw new ClientError(401, 'Invalid login, please try again.');
+    }
+    const payload = { userId, username };
+    const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+    res.json({ token, user: payload });
+  } catch (err) {
+    next(err);
   }
 });
 
